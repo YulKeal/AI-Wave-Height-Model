cb3aed32e88ee02e60d51db9914eebcd9aeef953
YulKeal <xwangcs@126.com>
2024-08-12 17:12:08 +0800

    Update ModelEnsemble.py

--- global_ocean/ModelEnsemble.py
+++ global_ocean/ModelEnsemble.py
@@ -2,7 +2,7 @@ import numpy as np
 from tqdm import tqdm
 from sklearn.linear_model import LinearRegression
 import os
-import plotfig
+
 lat=281
 lon=720
 
@@ -14,8 +14,8 @@ def train():
 
     for year in range(2000,2018):
 
-        data1=np.load('D:/EartherFormerResults/epochensemble/plot_era5_global_HR0.5_4ptensemble_'+str(year)+'.npz')
-        data2=np.load('D:/EartherFormerResults/Unet_Results/plot_era5_unet_global__'+str(year)+'.npz')
+        data1=np.load('./EartherFormerResults/epochensemble/plot_era5_global_HR0.5_4ptensemble_'+str(year)+'.npz')
+        data2=np.load('./Unet_Results/plot_era5_unet_global__'+str(year)+'.npz')
 
         mask=np.load('mask_global_HRSWH.npz')['mask']
 
@@ -89,26 +89,25 @@ def get_state(out_data,label_data):
     label_mean = np.mean(label_data, 0)
     relative_rmse_figure = rmse_figure / np.where(label_mean == 0, 1, label_mean)
 
-    # 初始化一个与纬度和经度维度相同的数组，用于存储相关系数
     correlation_coefficients = np.zeros([lat, lon])
     bias = np.zeros([lat, lon])
 
     bias = np.mean(rmse_figure_data[0, :] - rmse_figure_data[1, :], axis=0)
 
-    # 遍历每个地理位置
+
     for i in range(rmse_figure_data.shape[2]):
         for j in range(rmse_figure_data.shape[3]):
-            # 提取该位置的预测值和真实值
+     
             predictions = rmse_figure_data[0, :, i, j]
             actuals = rmse_figure_data[1, :, i, j]
             if np.all(actuals == 0) == True:
                 correlation_coefficients[i, j] = 1
                 continue
 
-            # 计算相关系数
+  
             correlation = np.corrcoef(predictions, actuals)[0, 1]
 
-            # 将相关系数存储在数组中
+
             correlation_coefficients[i, j] = correlation
 
     return rmse_figure, relative_rmse_figure, correlation_coefficients, bias
@@ -127,11 +126,7 @@ def test(year=2020):
 
     print("year:{}".format(year))
 
-    #
-    # if (data1['label_data']==data2['label_data']).all():
-    #     print("year:{} pass".format(year))
-    # else:
-    #     print("Error")
+
 
     label_data = data1['label_data']
 
@@ -174,14 +169,6 @@ def test(year=2020):
              axis_lat=axis_lat,
              axis_lon=axis_lon)
 
-    plotfig.Plot_Scatter(out_data, label_data, time_step=None, filename=plot_path+str(year) + 'global_sc')
-    plotfig.Plot_RMSE_N(rmse_figure_data, axis_lat, axis_lon, filename=plot_path+str(year) + 'global_rmse',cbname='RMSE (m)')
-    plotfig.Plot_RMSE_N(correlation_coefficients, axis_lat, axis_lon, filename=plot_path+str(year) + 'global_cor',
-                         figurename='Spatial Correlation Map',cbname='Correlation Coefficients')
-    plotfig.Plot_RMSE_N(rrmse_figure_data, axis_lat, axis_lon,
-                            filename=plot_path+str(year) + '_rrmse', figurename='Relative RMSE Spatial Distribution Map',cbname='Relative RMSE')
-    plotfig.Plot_RMSE_N(bias, axis_lat, axis_lon,
-                            filename=plot_path+str(year) + '_bias', figurename='Bias Spatial Distribution Map',cbname='Bias (m)')
 
 #train()
 # for year in range(2021,2023):
